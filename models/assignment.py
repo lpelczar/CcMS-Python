a236e3ef3a7d9e2bdb530d40b42da4f55b2925cd
creat73 <nikill1125@gmail.com>
2017-11-07 11:29:48 +0100

    Add save assignments to and load from file

--- models/assignment.py
+++ models/assignment.py
@@ -2,13 +2,24 @@ import view/assignment_view
 
 class Assignment():
     def __init__(self):
-        self.assignments = []
+        self.assignments = {}
 
     def add_assignment(self):
         pass
 
-    def save_to_file(self):
-        pass
+    @staticmethod
+    def save_to_file(filename:str = 'assignments.txt'):
+        dictionary = self.assignments
+        with open(filename, 'w') as file:
+            for key, value in dictionary.items():
+                file.write(str(value) + ';' + str(key) + '\n')
 
-    def load_from_file(self):
-        pass
+    @staticmethod
+    def load_from_file(filename:str = 'assignments.txt'):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                splitted_line = line.split(';')
+                title = splitted_line[0]
+                description = splitted_line[1]
+                self.assignments[title] = description
--- views/assignment_view.py
+++ views/assignment_view.py
@@ -1,5 +1,6 @@
 class AssignmentView:
-    def new_assignment_input(self, is_title=True):
+    @staticmethod
+    def new_assignment_input(is_title=True):
         if is_title:
             input_message = 'Name your assignment: '
         else:
