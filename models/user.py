d8f52a8ff4642f4650e7c33e4d7c3811e1334cc3
lpelczar <lukepelczar@gmail.com>
2017-11-07 18:38:14 +0100

    Add setters and getters to user

--- models/user.py
+++ models/user.py
@@ -16,5 +16,35 @@ class User:
         self.phone_number = phone_number
         self.email = email
 
+    def get_login(self):
+        return self.login
+
+    def get_password(self):
+        return self.password
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_email(self):
+        return self.email
+
+    def get_name(self):
+        return self.name
+
+    def set_login(self, login):
+        self.login = login
+
+    def set_password(self, password):
+        self.password = password
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    def set_email(self, email):
+        self.email = email
+
+    def set_name(self, name):
+        self.name = name
+
     def __str__(self):
         return 'Name: {} Phone: {} Email: {}'.format(self.name, self.phone_number, self.email)
