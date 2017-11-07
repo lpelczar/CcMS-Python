c8ecda6159666a52335fbaab1a6a3336a9e8e2e5
lpelczar <lukepelczar@gmail.com>
2017-11-07 13:15:27 +0100

    Add show students method

--- models/employee.py
+++ models/employee.py
@@ -5,3 +5,10 @@ class Employee(User):
 
     def __init__(self, *args, **kwargs):
         super(Employee, self).__init__(*args, **kwargs)
+
+    def show_students():
+        """
+        :return: list of Students
+        """
+        users = UserContainer.get_users_list()
+        return [person for person in users if isinstance(person, Student)]
