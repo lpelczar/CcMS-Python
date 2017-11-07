13d505742c0f67ef1a97de69d6362a211faaa06c
lpelczar <lukepelczar@gmail.com>
2017-11-07 11:08:05 +0100

    Add getting student grades

--- models/student.py
+++ models/student.py
@@ -4,7 +4,13 @@ from user import User
 class Student(User):
 
     def __init__(self, *args, **kwargs):
-        self.submissions = {}
+        self.submissions = {}  # Dictionary with key: Assignment, value: Grade
         self.group = None
         self.attendance = 0
         super(Student, self).__init__(*args, **kwargs)
+
+    def get_grades(self):
+        """
+        :return: list -> student grades
+        """
+        return list(self.submissions.values())
