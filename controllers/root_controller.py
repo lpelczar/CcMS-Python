edc99720ae0b211a8de421156d99bea10756a89f
lpelczar <lukepelczar@gmail.com>
2017-11-08 10:32:17 +0100

    Implement get instance method in root controller

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -3,10 +3,23 @@ from manager_controller import ManagerController
 from employee_controller import EmployeeController
 from student_controller import StudentController
 
+
 class RootController:
+
+    INSTANCE = None
 
     def __init__(self):
         self.mentor_controller = MentorController()
         self.manager_controller = ManagerController()
         self.employee_controller = EmployeeController()
         self.student_controller = StudentController()
+
+    @classmethod
+    def get_instance(cls):
+        """
+        Returns the singleton instance of Controller
+        :return: None
+        """
+        if cls.INSTANCE is None:
+            cls.INSTANCE = RootController()
+        return cls.INSTANCE
