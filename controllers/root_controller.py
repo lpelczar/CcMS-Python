603b52a88c5c1c297dfd0796611beff7bcb03bb7
lpelczar <lukepelczar@gmail.com>
2017-11-08 13:13:03 +0100

    Add gitignore and modify root controller

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -37,6 +37,7 @@ class RootController:
         Main loop for the program
         """
         RootView.display_starting_screen()
+        RootView.display_main_menu()
         while True:
             option = input('Choose option: ')  # Todo -> move input to RootView
             os.system('clear')
@@ -51,9 +52,10 @@ class RootController:
         """
         Handle creating new User
         """
-        RootView.display_sing_menu(True)
+        RootView.display_sign_menu(True)
         while True:
-            login, password = RootView.display_sign_up_menu()
+            login = RootView.create_user_login()
+            password = RootView.create_user_password()
             if self.user_container.get_user(login, password):
                 RootView.display_user_already_exists()  # Todo -> in RootView 'User already exists!'
                 continue
