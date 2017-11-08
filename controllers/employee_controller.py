36d49f5a1e9d903528ec01599c0e484872b24918
creat73 <nikill1125@gmail.com>
2017-11-08 13:44:21 +0100

    Add show students

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -1,2 +1,19 @@
+from models.user_container import UserContainer
+from views.employee_view import EmployeeView
+
+
 class EmployeeController:
-    pass
+
+    def start(self):
+        exit_program = False
+        while not exit_program:
+            option = EmployeeView.menu()
+            if option == "1":
+                self.show_students()
+            elif option == "2":
+                exit_program = True
+
+    @staticmethod
+    def show_students():
+        students_list = UserContainer.get_students_list()
+        EmployeeView.display_students_list(students_list)
