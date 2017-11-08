4a346e6bb8dd60d8d1b9b089ac0ed8c3d2a74e07
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-11-08 10:14:00 +0100

    Add staticmethod to sing up new user with email and password

--- views/root_view.py
+++ views/root_view.py
@@ -1,18 +1,22 @@
 import os
+import string
 
 
 class RootView:
 
-    def clear_screen(self):
-        self.os.system('clear')
-
-    def display_starting_screen(self, file_name='welcome_screen.txt'):
-        self.clear_screen()
+    @staticmethod
+    def display_starting_screen(file_name='welcome_screen.txt'):
+        """
+        Argument: str ---> with filename to reader
+        Return: None
+        """
+        os.system('clear')
         with open(file_name) as f:
             reader = f.read()
         print(reader)
 
-    def display_main_menu(self):
+    @staticmethod
+    def display_main_menu():
         welcome_information = '\nWelcome in Canvas, patch 0.-2XYZ.4C version.'
         exit_program = '\n0. Exit'
         menu_options = ['Sing in', 'Sing up']
@@ -26,5 +30,34 @@ class RootView:
             number_option += 1
         print(exit_program)
 
-    def display_sing_in_menu(self):
-        self.clear_screen()
+    @staticmethod
+    def display_sing_menu():
+        os.system('clear')
+        min_lenght_user_input = 1
+        max_lenght_user_input = 30
+        information_to_creat_new_user = '\nAs new user of our platform you need to sing up with ur email and unique password!'
+        user_email = ''
+        user_password = ''
+
+    @staticmethod
+    def display_sing_up_menu():
+        print(information_to_creat_new_user)
+        while user_email > min_lenght_user_input and user_email < max_lenght_user_input:
+            user_email = input('Enter your email adress(it cant be longer than 30 characters): ')
+
+        correct_input = False
+        while user_password > min_lenght_user_input and user_password < max_lenght_user_input and correct_input:
+            user_password = input('Enter your password(it must contain 1 big character and digit, it cant be longer than 30 characters): ')
+
+            for sign in user_password:
+                if sign.isdigit():
+                    for sign in user_password:
+                        if sign.isupper():
+                            correct_input = True
+
+        login_password = (user_email, user_password)
+        return login_password
+
+    @staticmethod
+    def display_sing_in_menu():
+        pass
