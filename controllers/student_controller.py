17c91867d259e651e038fff86b27a329bded5064
siemaeniu500 <siemaeniu500@gmail.com>
2017-11-08 20:42:22 +0100

    implement StudentController class

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,8 +1,8 @@
-import os
+import os, traceback
 from models.student import Student
 from models.user_container import UserContainer
 from views.student_view import StudentView
-
+from models.assignment_container import AssignmentContainer
 
 class StudentController:
 
@@ -19,15 +19,24 @@ class StudentController:
         while not should_exit:
             try:
                 os.system('clear')
-                StudentView.display_student_menu()
+                StudentView.display_student_menu(self.student.login, 'Student')
                 user_input = StudentView.get_user_input('Choose an option: ')
                 if user_input == '1':
                     self.submit_assignment()
                     StudentView.display_submission_result()
                 elif user_input == '2':
                     self.show_student_grades()
-            except Exception as e:
-                print(e)
+                elif user_input == '3':
+                    should_exit == True
+            except IndexError:
+                StudentView.print_wrong_assignment_id_error()
+            except ValueError:
+                StudentView.print_wrong_assignment_id_error()
+            except Exception:
+                tb = traceback.format_exc()
+                print(tb)
+                input()
+
 
 
     @classmethod
@@ -44,14 +53,23 @@ class StudentController:
         """
         Show grades for student with given student login
         """
+        assignments_with_grades = [assignment for assignment in self.student.assignments if
+                                         assignment.grade != None]
         if not self.student.assignments:
             StudentView.print_user_have_no_grades()
+        assignments_as_strings_list = []
+        for assignment in assignments_with_grades:
+            grade_with_assignment_name = 'Grade: {}, Assignment title: {}'.format(assignment.grade,assignment.title)
+            assignments_as_strings_list.append(grade_with_assignment_name)
+        StudentView.display_user_grades(assignments_as_strings_list)
+
 
     def submit_assignment(self):
         """
         Add new key to submissions with assignment_name
         """
-        assignment_without_submission = [assignment for assignment in self.student.assignments if assignment.submission == None]
+        assignment_without_submission = [assignment for assignment in self.student.assignments if
+                                         assignment.submission == None]
         if not assignment_without_submission:
             StudentView.print_user_assignments_list_empty_error()
             return
@@ -60,3 +78,16 @@ class StudentController:
         submission_id = int(StudentView.get_user_input('Type submission ID: '))
         submission_content = StudentView.get_user_input('Type link to github repository: ')
         self.student.add_submission(submission_id, submission_content)
+
+    def update_user_assignments(self):
+        for global_assignment in AssignmentContainer.get_instance().get_assignments_list():
+            add_assignment = True
+            for user_assignment in self.student.assignments:
+                if user_assignment.name == global_assignment.name:
+                    add_assignment = False
+                    break
+            if add_assignment:
+                self.student.add_student_assigment(global_assignment.deadline, global_assignment.title,
+                                                   global_assignment.description)
+
+
