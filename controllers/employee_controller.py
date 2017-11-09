703dc2284977e8eaa18873c59efc2ed45d3d3235
creat73 <nikill1125@gmail.com>
2017-11-09 14:04:53 +0100

    Fix display looks and menu input error

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -1,5 +1,6 @@
 from models.user_container import UserContainer
 from views.employee_view import EmployeeView
+import os
 
 
 class EmployeeController:
@@ -7,11 +8,14 @@ class EmployeeController:
     def start(self):
         exit_program = False
         while not exit_program:
+            os.system('clear')
             option = EmployeeView.menu()
             if option == "1":
                 self.show_students()
             elif option == "2":
                 exit_program = True
+            else:
+                EmployeeView.display_input_error()
 
     @staticmethod
     def show_students():
--- views/employee_view.py
+++ views/employee_view.py
@@ -1,5 +1,9 @@
 class EmployeeView:
     @staticmethod
+    def display_input_error():
+        print('Wrong input!')
+
+    @staticmethod
     def menu():
         return input('''
         Choose option:
@@ -10,5 +14,5 @@ class EmployeeView:
     @staticmethod
     def display_students_list(students_list):
         for student in students_list:
-            print(students_list.index(student) + student.get_name() + student.group + '\n' + student.get_phone_number()
-                  + '\n' + student.get_email())
+            print('Index: ' + students_list.index(student) + ' Name: ' + student.get_name() + ' Group: ' + student.group
+                  + '\n'+ 'Phone number:' + student.get_phone_number() + ' Email: ' '\n' + student.get_email())
