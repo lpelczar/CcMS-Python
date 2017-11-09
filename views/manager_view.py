cd544a5e9f2696e4465e1983feb7072456d32184
lpelczar <lukepelczar@gmail.com>
2017-11-09 11:36:42 +0100

    Refactor mentor controller methods and views

--- views/manager_view.py
+++ views/manager_view.py
@@ -1,34 +1,52 @@
 class ManagerView:
 
     @staticmethod
-    def display_manager_menu():
-        print('1. Promote user to Mentor\n' +
+    def display_manager_menu(user_login, role):
+        greeting_message = 'Logged as {} ({}) \n'.format(user_login, role)
+        print(greeting_message +
+              '1. Promote user to Mentor\n' +
               '2. Remove Mentor\n' +
               '3. Edit Mentor data\n' +
               '4. Display list of Mentors\n' +
-              '5. Display list of students')
+              '5. Display list of Students')
 
     @staticmethod
-    def display_users(self, users):
+    def get_user_input(prompt: str):
+        return input(prompt)
+
+    @staticmethod
+    def display_users(users):
+        print('')
         for k, v in enumerate(users):
-            print(k+1 + '. ' + str(v))
+            print(str(k + 1) + '. ' + 'Login: ' + v.get_login() + ' Name: ' + v.get_name() + ' Role: ' + v.__class__.__name__)
 
     @staticmethod
-    def get_promotion_input(self):
+    def display_students(students):
+        print('')
+        for k, v in enumerate(students):
+            print(str(k + 1) + '. ' + 'Login: ' + v.get_login() + ' Name: ' + v.get_name() + ' Role: ' + v.__class__.__name__)
+
+        input('\nPress ENTER to continue')
+
+    @staticmethod
+    def get_promotion_input():
         return input('Enter login of the user which do you want to promote to Mentor: ')
 
     @staticmethod
-    def get_user_remove_input(self):
+    def get_user_remove_input():
         return input('Enter login of the user which do you want to remove: ')
 
     @staticmethod
-    def display_user_promoted(self, user):
+    def display_user_promoted(user):
         print('User: {} has been promoted to Mentor'.format(user.get_login()))
+        input('\nPress ENTER to continue')
 
     @staticmethod
-    def display_user_deleted(self, user):
+    def display_user_deleted(user):
         print('User: {} has been deleted'.format(user.get_login()))
+        input('\nPress ENTER to continue')
 
     @staticmethod
-    def display_user_not_found(self):
+    def display_user_not_found():
         print('User with that name not found!')
+        input('\nPress ENTER to continue')
