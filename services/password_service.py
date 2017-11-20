f7b9e6bf108fa4966f537fa66bf14a7988fc80f8
lpelczar <lukepelczar@gmail.com>
2017-11-20 13:57:31 +0100

    Delete passlib, add encryption functions to password service

--- services/password_service.py
+++ services/password_service.py
@@ -1,2 +1,14 @@
+import hashlib
+
+
 class PasswordService:
-    pass
+
+    @staticmethod
+    def encrypt_password(password):
+        secure_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        return secure_hash
+
+    @staticmethod
+    def verify_password(password, secure_hash):
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        return True if password_hash != secure_hash else False
