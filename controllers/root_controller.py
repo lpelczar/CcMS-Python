91f558c6c0a73f76e716e945d98a756359c89bba
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-11-20 15:04:33 +0100

    Clean code, root view, while loop using variables instead of true/false

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -10,7 +10,6 @@ from models.mentor import Mentor
 from models.manager import Manager
 from models.employee import Employee
 from controllers.key_getch import getch
-import sys
 import os
 
 
@@ -23,63 +22,81 @@ class RootController:
         """
         Main loop for the program
         """
+        exit_program = True
         RootView.display_animate_starting_screen()
-        while True:
+
+        while exit_program:
             os.system('clear')
+
             RootView.display_main_menu_screen()
             RootView.display_main_menu()
-            option = getch()  # Todo -> move input to RootView
+            option = getch()
+
             if option == '1':
                 self.handle_sign_in()
+
             elif option == '2':
                 self.handle_sign_up()
+
             elif option == '0':
                 UserContainer.get_instance().save_users_to_file()
-                sys.exit()
+                exit_program = False
 
     def handle_sign_up(self):
         """
         Handle creating new User
         """
         RootView.display_sign_menu(True)
-        while True:
+
+        sing_up = True
+        while sing_up:
             login = RootView.create_user_login()
             password = RootView.create_user_password()
 
             if self.user_container.get_user(login, password):
                 RootView.display_user_already_exists()
-                continue
+
             else:
                 phone_number = RootView.create_user_phone_number()
                 email = RootView.create_user_email()
                 name = RootView.add_user_name()
+
                 self.user_container.add_user(User(login, password, phone_number, email, name))
                 RootView.display_user_created(login, password, phone_number, email, name)
-                break
+
+                sing_up = False
 
     def handle_sign_in(self):
         """
         Handle logging to existing user
         """
         RootView.display_sign_menu(False)
-        while True:
+
+        sing_in = True
+        while sing_in:
             login, password = RootView.get_user_login_password()
             user = self.user_container.get_user(login, password)
+
             if user:
+
                 if isinstance(user, Student):
                     StudentController(user).start()
-                    sys.exit()
+                    sing_in = False
+
                 elif isinstance(user, Mentor):
                     MentorController().start()
-                    sys.exit()
+                    sing_in = False
+
                 elif isinstance(user, Manager):
                     ManagerController(user).start()
-                    sys.exit()
+                    sing_in = False
+
                 elif isinstance(user, Employee):
                     EmployeeController().start()
-                    sys.exit()
+                    sing_in = False
+
                 elif isinstance(user, User):
                     RootView.display_error_user_singin()
-                    sys.exit()
+                    sing_in = False
             else:
                 RootView.display_user_not_exist()
