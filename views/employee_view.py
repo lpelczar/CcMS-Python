db3ec2426febcff23f84442494c900c71ef3a8be
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-11-21 12:50:18 +0100

    Colored employee view class

--- views/employee_view.py
+++ views/employee_view.py
@@ -1,28 +1,30 @@
+from colorful_view import ColorfulView
 import os
 
 
 class EmployeeView:
     @staticmethod
     def display_input_error():
-        print('Wrong input!')
+        print(ColorfulView.format_string_to_red('Wrong input!'))
 
     @staticmethod
     def display_menu():
-        return input('''
+        menu = ColorfulView.format_string_to_yellow('''
         Choose option:
         1. Show students
         2. Exit
         ''')
+        return input(menu)
 
     @staticmethod
     def display_students_list(students_list):
         os.system('clear')
         for student in students_list:
             if not student.group:
-                group_str = 'Not assigned'
+                group_str = ColorfulView.format_string_to_red('Not assigned')
             else:
                 group_str = student.group
-            print('Index: ' + str(students_list.index(student)) + ' Name: ' + student.get_name() + ' Group: ' +
+            print(ColorfulView.format_string_to_green('Index: ' + str(students_list.index(student)) + ' Name: ' + student.get_name() + ' Group: ' +
                   group_str + '\n' + 'Phone number:' + str(student.get_phone_number()) + ' Email: ' + '\n'
-                  + student.get_email())
+                  + student.get_email()))
         input('Press enter to return')
