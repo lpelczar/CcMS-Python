ca69e2a32810bfea52f716a8b1c888392dba3580
lpelczar <lukepelczar@gmail.com>
2017-11-21 11:37:43 +0100

    Implement getting password with asterisks

--- services/password_service.py
+++ services/password_service.py
@@ -1,4 +1,7 @@
 import hashlib
+import os
+import time
+from controllers.key_getch import getch
 
 
 class PasswordService:
@@ -7,3 +10,20 @@ class PasswordService:
     def encrypt_password(password):
         secure_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
         return secure_hash
+
+    @staticmethod
+    def get_password_with_asterisks():
+        password = []
+        password_created = False
+        while not password_created:
+            os.system('clear')
+            print('*' * (len(password if password else 1) - 1) + password[-1] if password else '')
+            print('Enter password: ', end='')
+            time.sleep(0.3)
+            os.system('clear')
+            print('*' * len(password))
+            x = getch()
+            if x == '\r':
+                password_created = True
+            password.append(x)
+        return ''.join(password)
